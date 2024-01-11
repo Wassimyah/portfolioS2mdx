73e096ce2c436c7a53d02435eb1bb74b4e3d5a25
Wassim Yahiaoui <114857379+Wassimyah@users.noreply.github.com>
2024-01-11 12:00:41 +0100

    veel edits

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-lg duration-500 text-zinc-300 hover:text-zinc-900"
             >
               {item.name}
             </Link>
